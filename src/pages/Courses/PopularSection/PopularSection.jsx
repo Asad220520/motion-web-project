37e6c1cc60adb1e7b9f9b5fdb3f5a5e4d97f3aa7
chyngyz-sultanaliev <sultanalievcyngyz98@gmail.com>
2025-06-07 23:11:41 +0600

    Coures

---
+++ src/pages/Courses/PopularSection/PopularSection.jsx
@@ -0,0 +1,247 @@
+import React, { useState } from "react";
+import "./PopularSection.scss";
+import Button from "@/components/Button";
+import classNames from "classnames";
+import img from "../../../assets/images/productCard.png";
+import img1 from "../../../assets/icons/card-icon-1.svg";
+import img2 from "../../../assets/icons/card-icon-2.svg";
+import img3 from "../../../assets/icons/card-icon-3.svg";
+import ProductCard from "../ProductCard/ProductCard";
+const PopularSection = () => {
+  const [slice, setSlice] = useState(4);
+  const categories = [
+    "Все Курсы",
+    "Управление Компанией",
+    "Командообразование",
+    "Маркетинг",
+    "Продажи",
+  ];
+
+  const [activeCategory, setActiveCategory] = useState("Все Курсы");
+
+  const courses = [
+    {
+      id: 1,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Управление Компанией",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Командообразование",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Маркетинг",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Управление Компанией",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Командообразование",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+    {
+      id: 6,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Маркетинг",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+    {
+      id: 7,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Управление Компанией",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+    {
+      id: 8,
+      title: "Как ставить о оценивать задачи",
+      desc: "Мы ориентируемся на эргономику иты где работаешь. Это всего лишь нажатие клавиши.",
+      img: img,
+      category: "Командообразование",
+      info: [
+        {
+          text: "22ч 30мин",
+          img: img1,
+        },
+        {
+          text: "64 уроков",
+          img: img2,
+        },
+        {
+          text: "Прогресс",
+          img: img3,
+        },
+      ],
+    },
+  ];
+
+  const filteredCourses =
+    activeCategory === "Все Курсы"
+      ? courses
+      : courses.filter((course) => course.category === activeCategory);
+
+  return (
+    <section id="popularSection">
+      <div className="container">
+        <div className="popularSection">
+          <div className="popularSection--title">
+            <h1>Популярные курсы</h1>
+            <p>
+              Мы предоставляем множество функций, которые вы можете
+              использовать. Постепенное накопление информации.
+            </p>
+          </div>
+          {/* Категории */}
+          <div className="popularSection--categories">
+            {categories.map((cat, index) => (
+              <a
+                key={index}
+                onClick={() => setActiveCategory(cat)}
+                className={classNames("popularSection--categories__btn", {
+                  "is-active": activeCategory === cat,
+                })}
+              >
+                {cat}
+              </a>
+            ))}
+          </div>
+          {/* Сетка курсов */}
+          <div className="popularSection--grid">
+            {filteredCourses.slice(0, slice).map((course) => (
+              <ProductCard el={course} key={course.id} />
+            ))}
+          </div>
+          {filteredCourses.length >= 4 ? (
+            <button
+              onClick={() =>
+                filteredCourses.length > slice
+                  ? setSlice(slice + filteredCourses.length)
+                  : setSlice(4)
+              }
+              className="popularSection--btn"
+            >
+              {filteredCourses.length > slice ? "Смотреть больше" : "Закрыть"}
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PopularSection;
