994c32f25e57da595289e3c324a93a27b4927d55
Asad <130597420+Asad220520@users.noreply.github.com>
2025-06-08 06:14:53 +0600

    Ubdate code (#4)
    * add slider
    
    * add slider
    
    ---------
    
    Co-authored-by: Asadhkm <asadhkm4@gmail.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Button from "@/components/Button";
-import Search from "../../components/Search/Search";
+import Reviews from "./Reviews/Reviews";
+import Search from "@/components/Search/Search";
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
         <div className="home__inner">
-          <h1 className="h6">--color-text</h1>
-          <Button label="fdsf" />
+          <Reviews />
           <Search />
         </div>
       </div>
---
+++ src/pages/Home/Reviews/ReviewsCard/ReviewsCard.jsx
@@ -0,0 +1,27 @@
+import "./ReviewsCard.scss";
+import rivo from "@/assets/images/revios.svg";
+import { BiSolidStar } from "react-icons/bi";
+
+const ReviewsCard = ({ name, location, rating, content }) => {
+  return (
+    <div className="reviewsCard">
+      <div className="reviewsCard__header">
+        <div className="reviewsCard__user">
+          <img src={rivo} alt="User" className="reviewsCard__avatar" />
+          <div className="reviewsCard__user-info">
+            <h5 className="reviewsCard__name">{name}</h5>
+            <span className="reviewsCard__location">{location}</span>
+          </div>
+          <div className="reviewsCard__rating">
+            {rating} <BiSolidStar color="orange" />
+          </div>
+        </div>
+      </div>
+      <div className="reviewsCard__content">
+        <p className="reviewsCard__text">«{content}»</p>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewsCard;
