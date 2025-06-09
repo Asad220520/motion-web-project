ba47930558ad3bc2539cadc830d4ab322967ddc3
uhckunbek006 <uajiniyazov@gmail.com>
2025-06-09 14:37:51 +0600

    AboutUs (#5)
    Co-authored-by: uchkunbek006 <ajiniyazovuchkunbek@gmail.com>
    Co-authored-by: Asad <130597420+Asad220520@users.noreply.github.com>

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -6,17 +6,15 @@ const Search = () => {
     <div id="search">
       <div className="container">
         <div className="search">
-          <center>
-            <h1>Присоединяйся к нам </h1>
-            <h2>
-              Мы предоставляем множество функций, которые вы можете <br />
-              использовать. Постепенное накопление информация{" "}
-            </h2>
-            <div className="search--input">
-              <input type="text" placeholder="Твой Email" />
-              <button>Подписка</button>
-            </div>
-          </center>
+          <h1>Присоединяйся к нам </h1>
+          <h2>
+            Мы предоставляем множество функций, которые вы можете <br />
+            использовать. Постепенное накопление информация{" "}
+          </h2>
+          <div className="search--input">
+            <input type="text" placeholder="Твой Email" />
+            <button>Подписка</button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Button from "@/components/Button";
+import Search from "../../components/Search/Search";
+import AboutUs from "../AboutUs/AboutUs";
 import Reviews from "./Reviews/Reviews";
 import Search from "@/components/Search/Search";
 const Home = () => {
@@ -6,6 +9,9 @@ const Home = () => {
     <div className="home">
       <div className="container">
         <div className="home__inner">
+          <h1 className="h6">--color-text</h1>
+          <Button label="fdsf" />
+          <AboutUs />
           <Reviews />
           <Search />
         </div>
